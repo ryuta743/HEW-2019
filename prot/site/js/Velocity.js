053a1e268e7c7603e84b4842735e6c67520cbe09
mitsuru170 <mitsuru326323@icloud.com>
2019-07-16 10:55:16 +0900

    メインビジュアルプロト仮
    ハンバーガーメニューとメインの画像の下に何か作る

---
+++ prot/site/js/Velocity.js
@@ -0,0 +1,10 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
+<script type="text/javascript">
+(function($) {
+    $(function () {
+        $('#nav-toggle').on('click', function() {
+            $('body').toggleClass('open');
+        });
+    });
+})(jQuery);
+</script>
