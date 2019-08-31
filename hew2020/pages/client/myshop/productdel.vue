6f67afa213f2a508e116326dea2bc797f02cbae5
takashi <kuwataka07@gmail.com>
2019-08-31 10:59:03 +0900

    商品編集画面完成

--- hew2020/pages/client/myshop/productdel.vue
+++ hew2020/pages/client/myshop/productdel.vue
@@ -7,7 +7,7 @@
         <v-btn color="grey" @click="$router.push('/client/myshop/myshop')">管理画面へ戻る</v-btn>
       </v-toolbar>
 
-    　<v-card>
+    　<v-card v-if="!editmode">
           <v-card-text>
               
           <v-layout row wrap>
@@ -21,7 +21,7 @@
               <v-card hover>
                 <v-card-text>
                     <v-btn color="error" outlined @click="targetSel(index)"><v-icon hover color="red">mdi-close</v-icon></v-btn>
-                    <v-btn color="info" outlined><v-icon hover>mdi-pencil</v-icon></v-btn>
+                    <v-btn color="info" outlined @click="editTargetSel(index)"><v-icon hover>mdi-pencil</v-icon></v-btn>
                   </v-card-text>
                 <v-img :src="item.src" @click="$router.push(`/customer/product/${item.title}`)"></v-img>
                 <v-card-text style="font-weight: bold;height: 10px;" @click="$router.push(`/customer/product/${item.title}`)">{{item.title}}</v-card-text>
@@ -59,6 +59,94 @@
       </v-card>
     </v-content>
 
+    <v-content v-if="editmode">
+        <v-card>
+        <v-card-title primary-title>商品変更点入力</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+          <v-flex xs12 md5>
+            <h3 style="padding-bottom: 10px;">商品イメージ変更</h3>
+            <v-file-input label="File input" filled prepend-icon="mdi-camera"></v-file-input>
+          </v-flex>
+          <v-divider style="padding-bottom: 20px;"></v-divider>
+
+          <h3 style="padding-bottom: 10px;">商品名変更</h3>
+          <v-layout row wrap>
+            <v-flex xs12 md5>
+              <v-text-field label="商品名(日本)" outlined></v-text-field>
+            </v-flex>
+            <v-flex xs12 md5>
+              <v-text-field label="商品名(英語)" outlined></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-divider style="padding-bottom: 20px;"></v-divider>
+          <v-flex xs5 md3>
+            <h3 style="padding-bottom: 10px;">販売価格変更</h3>
+            <v-layout row wrap align-center>
+              <v-text-field label="価格" outlined type="number"></v-text-field>
+              <h2>円</h2>
+            </v-layout>
+          </v-flex>
+          <v-divider style="padding-bottom: 20px;"></v-divider>
+          <h3 style="padding-bottom: 10px;">安全在庫数変更</h3>
+          <v-layout row wrap align-center>
+            <v-flex xs5 md2>
+              <v-text-field type="number" label="安全在庫数" outlined></v-text-field>
+            </v-flex>
+            <h2>個</h2>
+          </v-layout>
+          <v-divider style="padding-bottom: 20px;"></v-divider>
+          <v-flex xs12 md12>
+            <h3 style="padding-bottom: 10px;">商品説明変更</h3>
+            <v-text-field outlined label="商品説明"></v-text-field>
+          </v-flex>
+          <v-divider style="padding-bottom: 20px;"></v-divider>
+          <h3 style="padding-bottom: 10px;">
+            商品につけるタグ
+            <span style="font-size: 15px;color: #999;">(4つまで)</span>
+          </h3>
+          <v-layout row wrap>
+            <v-flex xs10 md10>
+              <v-text-field outlined label="追加タグ" v-model="formData.tag"></v-text-field>
+            </v-flex>
+            <v-flex xs1 md1>
+              <v-btn color="info" style="width: 100%;height: 55px;" outlined @click="addTag">追加</v-btn>
+            </v-flex>
+            <v-flex xs12 md12>
+              <v-subheader>追加中のタグ</v-subheader>
+              <v-layout row wrap>
+                <v-flex xs6 md12>
+                  <v-chip
+                    @click="formData.tags.splice(index,1)"
+                    class="ma-2"
+                    color="primary"
+                    label
+                    text-color="white"
+                    v-for="(item, index) in formData.tags"
+                    :key="index"
+                  >
+                    <v-icon left>mdi-backspace</v-icon>
+                    {{item}}
+                  </v-chip>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-layout row wrap justify-end>
+            <v-flex xs12 md1>
+              <v-btn color="info" style="width: 100%;" @click="completedialog = true">変更</v-btn>
+            </v-flex>
+            <v-flex xs12 md1>
+              <v-btn style="width: 100%;" @click="editmode = false">キャンセル</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+
     <!-- 削除確認ダイアログ -->
     <v-dialog
         v-model="dialog"
@@ -81,6 +169,27 @@
             </v-card-actions>
         </v-card>
     </v-dialog>
+
+    <!-- completeダイアログ -->
+    <v-dialog
+        v-model="completedialog"
+        scrollable 
+        persistent :overlay="false"
+        max-width="500px"
+        transition="dialog-transition"
+    >
+     <v-card>
+         <v-card-text style="padding: 20px;">
+             <v-layout row wrap justify-center align-center>
+                 <h3><v-icon>mdi-check</v-icon></h3>
+                 <h3>変更が完了しました</h3>
+             </v-layout>
+             <v-layout row wrap justify-center>
+                 <v-btn color="success" @click="editComplete">OK</v-btn>
+             </v-layout>
+         </v-card-text>
+     </v-card>   
+    </v-dialog>
   </v-container>
 </template>
 
@@ -88,8 +197,14 @@
 export default {
   data() {
     return {
+      completedialog: false,
+      editmode: false,
     　dialog: false,
       killtarget: null,
+      formData: {
+        tag: "",
+        tags: ["陶器"]
+      },
       products: [
         {
           title: "陶器01",
@@ -153,6 +268,22 @@ export default {
           this.products.splice(this.target,1)
           this.dialog = false
           this.target = null
+      },
+      addTag() {
+      if (this.formData.tag != "") {
+        if (this.formData.tags.length < 4) {
+          this.formData.tags.push(this.formData.tag);
+          this.formData.tag = "";
+        }
+      }
+      },
+      editTargetSel(i){
+          this.edittarget = i
+          this.editmode = true
+      },
+      editComplete(){
+          this.completedialog = false
+          this.editmode = false
       }
   }
 };
