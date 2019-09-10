c4df0c5f1d028258f2b9348ac370ac4c6d28f1db
takashi <kuwataka07@gmail.com>
2019-09-10 16:29:13 +0900

    firebaseにトリミング画像をポスト

--- hew2020/pages/client/myshop/productadd.vue
+++ hew2020/pages/client/myshop/productadd.vue
@@ -1,14 +1,36 @@
 <template>
   <v-container grid-list-xs style="min-height: 80vh;">
-    <v-content v-if="check ==0">
+    <v-content v-if="check == 0">
       <v-card>
         <v-card-title primary-title>新規登録商品情報入力</v-card-title>
         <v-divider></v-divider>
         <v-card-text>
           <v-flex xs12 md5>
             <h3 style="padding-bottom: 10px;">登録商品イメージ</h3>
-            <v-file-input label="File input" filled prepend-icon="mdi-camera"></v-file-input>
           </v-flex>
+          <client-only>
+            <croppa
+              v-model="myCroppa"
+              placeholder="画像を選択"
+              :placeholder-font-size=20
+              :width=1200
+              :height=500
+              canvas-color="#000"
+              :file-size-limit="700000"
+              :remove-button-size="30"
+            ></croppa>
+              <div>
+                <v-btn color="info" @click="generateImage">トリミング</v-btn>
+                <v-btn color="primary" @click="upload">アップロード!</v-btn>
+              </div>
+          </client-only>
+          
+          <v-divider></v-divider>
+          <v-card-text　v-if="imgUrl">
+            <h2 style="padding-bottom: 10px;">トリミング後の画像</h2>
+            <img class="output" :src="imgUrl" width="1200" height="500" />
+            <!-- <img src="https://firebasestorage.googleapis.com/v0/b/tenshoku-9b0c8.appspot.com/o/images%2Ftest3?alt=media&token=3747dfca-14b7-4476-b20d-7d23f993aa49" width="1000" height="400" alt=""> -->
+          </v-card-text>
           <v-divider style="padding-bottom: 20px;"></v-divider>
 
           <h3 style="padding-bottom: 10px;">登録商品名</h3>
@@ -163,13 +185,23 @@
 </template>
 
 <script>
+import Vue from "vue";
+import Croppa from "vue-croppa";
+import "vue-croppa/dist/vue-croppa.css";
+
+
 export default {
   data() {
     return {
+      myCroppa: null, //v-model croppa
+        imgUrl: null, //アウトプットするurlってことかな
+        blob: null,
+        uploadedName: null,
+        uploadedImg: null,
       check: 0,
       formData: {
         tag: "",
-        tags: ["陶器"]
+        tags: ["陶器"],
       }
     };
   },
@@ -181,7 +213,50 @@ export default {
           this.formData.tag = "";
         }
       }
+    },
+    generateImage: function() {
+      var type = "image/jpeg";
+      let compressionRate = 0.2;
+      let url = this.myCroppa.generateDataUrl(type, compressionRate);
+      if (!url) {
+        alert("画像が選択されていません");
+        return;
+      }
+      this.imgUrl = url;
+      // DataURL のデータ部分を抜き出し、Base64からバイナリに変換してる
+      var bin = atob(url.split(",")[1]);
+      // 空の Uint8Array ビューを作る
+      var buffer = new Uint8Array(bin.length);
+      // Uint8Array ビューに 1 バイトずつ値を埋める
+      for (var i = 0; i < bin.length; i++) {
+        buffer[i] = bin.charCodeAt(i);
+      }
+      // Uint8Array ビューのバッファーを抜き出し、それを元に Blob を作る
+      this.blob = new Blob([buffer.buffer], { type: type });
+      console.log(this.blob);
+    },
+    upload() {
+      if (this.imgUrl == null) {
+        alert("画像をトリミングしてください");
+        return;
+      }
+      this.$store
+        .dispatch("persona/uploadImage", {
+          name: "test3",
+          file: this.blob
+        })
+        .then(url => {
+          // アップロード完了処理 (ローカルメンバに保存したり)
+          app.uploadedImg = url; //アップロードしたURLを返してもらってる
+          console.log(app.uploadedImg);
+        });
+      setTimeout(() => {
+        alert("アップロードに成功しました");
+      }, 2000);
     }
+  },
+  mounted() {
+    Vue.use(Croppa);
   }
 };
 </script>
