3265bfc8a4a0d066ed848d7cc02bc08f6076906d
takashi <kuwataka07@gmail.com>
2019-12-30 09:29:36 +0900

    注文一覧＆注文明細完成

--- hew2020/pages/client/myshop/order/_order.vue
+++ hew2020/pages/client/myshop/order/_order.vue
@@ -25,7 +25,8 @@
       </ul>
     </v-container>
     <v-container grid-list-xs style="min-height: 85vh;width: 85%;overflow-y: scroll;">
-      <v-content>
+      <v-btn color="info" icon :loading="loading" v-if="loading" large></v-btn>
+      <v-content v-if="!loading">
         <v-toolbar color="info">
           <v-toolbar-title style="color: white;">注文詳細 注文No.{{$route.params.order}}</v-toolbar-title>
           <div class="flex-grow-1"></div>
@@ -53,17 +54,21 @@
               <v-simple-table>
                 <thead>
                   <tr>
-                    <th style="color: #111;">商品名</th>
-                    <th style="color: #111;">個数</th>
-                    <th style="color: #111;">梱包状態</th>
+                    <th style="color: #111;text-align: center;">商品番号</th>
+                    <th style="color: #111;text-align: center;">商品名</th>
+                    <th style="color: #111;text-align: center;">個数</th>
+                    <th style="color: #111;text-align: center;">単価</th>
+                    <th style="color: #111;text-align: center;">梱包状態</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="(item, index) in products" :key="index">
-                    <td>{{item.name}}</td>
-                    <td>{{item.num}}</td>
-                    <td>
-                      <v-checkbox v-model="item.check"></v-checkbox>
+                  <tr v-for="(item, index) in details" :key="index">
+                    <td style="color: #111;text-align: center;">{{item.product_number}}</td>
+                    <td style="color: #111;text-align: center;">{{item.product_name}}</td>
+                    <td style="color: #111;text-align: center;">{{item.count}}</td>
+                    <td style="color: #111;text-align: center;">{{item.price}}円</td>
+                    <td style="display: flex;justify-content: center;box-sizing: border-box;padding-bottom:60px;">
+                      <v-checkbox v-model="item.proccess"></v-checkbox>
                     </td>
                   </tr>
                 </tbody>
@@ -72,7 +77,8 @@
           </v-card-text>
           <v-divider></v-divider>
           <v-card-actions>
-            <v-layout row wrap justify-center v-if="orderInfo.isCheck">
+            <!-- v-if="orderlist[0] ? checkStatus:''" -->
+            <v-layout row wrap justify-center v-if="orderlist[0] ? checkStatus:''">
               <v-btn color="success" @click="dialog = true">発送完了</v-btn>
               <v-btn color="white" @click="$router.push(`/client/myshop/order_print/${$route.params.order}`)">領収書発行</v-btn>
               <v-btn color="white" @click="$router.push(`/client/myshop/delivery/${$route.params.order}`)">納品書</v-btn>
@@ -125,9 +131,12 @@
 </template>
 
 <script>
+import {mapActions,mapGetters} from 'vuex'
 export default {
   data() {
     return {
+      shop_id: 1,
+      loading: true,
       dialog: false,
       complete_dialog: false,
       orderInfo: {
@@ -135,22 +144,24 @@ export default {
         date: 20190828,
         address: "~~~~",
         isCheck: true
-      },
-      products: [
-        {
-          name: "陶器01",
-          num: 2
-        },
-        {
-          name: "陶器02",
-          num: 1
-        },
-        {
-          name: "陶器03",
-          num: 1
-        }
-      ]
+      }
     };
+  },
+  async mounted() {
+    var r1 = this.getOrderlist({ wsid: this.shop_id });
+    var r2 = this.getOrderdetail({ wsid: this.shop_id,order_number: this.$route.params.order });
+    var r3 = await r1;
+    var r4 = await r2;
+    this.loading = false
+  },
+  methods: {
+    ...mapActions("workshop_manage", ["getOrderdetail","getOrderlist"])
+  },
+  computed: {
+    checkStatus(){
+      return this.orderlist[0].status==0 ? true:false
+    },
+    ...mapGetters("workshop_manage", ["workshop_data","orderlist","details"])
   }
 };
 </script>
--- hew2020/store/workshop_manage.js
+++ hew2020/store/workshop_manage.js
@@ -3,45 +3,63 @@ import moment from '~/plugins/moment';
 export const state = () => ({
     workshop_data: {},
     orderlist: [],
+    details: []
 })
 
 export const getters = {
     workshop_data: state => state.workshop_data,
     loading: state => state.loading,
     orderlist: state => state.orderlist,
+    details: state => state.details,
 }
 
 export const mutations = {
     setWorkshop(state, val) {
         state.workshop_data = val;
     },
-    setOrderlist(state,val){
+    setOrderlist(state, val) {
         state.orderlist = val
+    },
+    setOrderdetail(state, val) {
+        state.details = val
     }
 }
 
 export const actions = {
-    async getShopdata({ commit },{ wsid }) {
-        console.log('受け取ったデータ:' + wsid)
+    async getShopdata({ commit }, { wsid }) {
         try {
             const ws_inf = await this.$axios.$get(`http://133.18.194.128:5000/workshopManage/getShopdata?shop_id=${wsid}`);
-            console.log('取ってきたデータ:' + ws_inf)
-            console.log({ws_inf})
-            commit('setWorkshop',ws_inf[0])
+            commit('setWorkshop', ws_inf[0])
         } catch (error) {
-            if(error.response.status == 403){
+            if (error.response.status == 403) {
                 throw new Error("You don't have workshop")
             }
         }
     },
-    async getOrderlist({ commit },{ wsid }) {
-        console.log('受け取ったデータ:' + wsid)
+    async getOrderlist({ commit }, { wsid }) {
         try {
             const ws_inf = await this.$axios.$get(`http://133.18.194.128:5000/workshopManage/getOrderlist?shop_id=${wsid}`);
-            commit('setOrderlist',ws_inf)
+            commit('setOrderlist', ws_inf)
+        } catch (error) {
+            if (error.response.status === 403) {
+                throw new Error("You don't have workshop")
+            }
+        }
+    },
+    async getOrderdetail({ commit }, { wsid, order_number }) {
+        console.log('受け取ったデータ:' + wsid)
+        try {
+            console.log('aa')
+            const ws_inf = await this.$axios.$get(`http://133.18.194.128:5000/workshopManage/getOrderdetail?shop_id=${wsid}&order_number=${order_number}`);
+            if (ws_inf.length > 0) {
+                for (var i = 0; ws_inf.length > i; i++) {
+                    if (ws_inf[i].proccess == 1) ws_inf[i].proccess = true;
+                };
+            }
+            commit('setOrderdetail', ws_inf)
             console.log(ws_inf)
         } catch (error) {
-            if(error.response.status === 403){
+            if (error.response.status === 403) {
                 throw new Error("You don't have workshop")
             }
         }
