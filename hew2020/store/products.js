223939447cc4513dacaba8b604fd1958109a54fa
takashi <kuwataka07@gmail.com>
2020-01-27 15:16:45 +0900

    いじるるるう

--- hew2020/store/products.js
+++ hew2020/store/products.js
@@ -33,6 +33,7 @@ export const actions = {
         console.log('頑張れ正都！！');
         console.log(p_data.product_id);
         const product_details = await this.$axios.$get(`http://133.18.194.128:5000/product/get_details?id_data=${p_data.product_id}`);
+        product_details[0].product_img = `https://firebasestorage.googleapis.com/v0/b/tenshoku-9b0c8.appspot.com/o/images%2F${product_details[0].shop_id}%2Fproducts%2F${product_details[0].product_img}?alt=media`;
         console.log('おかえり');
         console.log(product_details[0],[0])
         commit('setProductdetails',product_details[0])
