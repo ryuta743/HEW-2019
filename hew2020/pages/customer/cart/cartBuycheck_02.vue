d1cda4a941471476696201704f664ba775ecfa22
katomasato <hankun0127taktak@gmail.com>
2020-02-21 00:13:03 +0900

    あとは頼んだ

--- hew2020/pages/customer/cart/cartBuycheck_02.vue
+++ hew2020/pages/customer/cart/cartBuycheck_02.vue
@@ -1,35 +1,50 @@
 <template>
       <div id="check_body">
+        <h1 style="margin-left:20px;">購入確認</h1>
+        <br><hr>
         <div id="box">
           <v-icon size="20px" color="success">mdi-check-circle</v-icon>
-          <div>
-            <h2>購入者情報</h2>
-            <!-- <p>購入者名：{{checkdata[0].sei}}{{checkdata[0].mei}}</p> -->
-            <p>お電話番号:{{checkdata[0].tel}}</p>
-            <p>メールアドレス：{{checkdata[0].mail}}</p>
-            <p>合計金額：:{{goukei}}円</p>
+          <h1 style="margin-left: 70px;">購入者情報確認</h1>
+          <div id="boss">
+            <div class="mboss">
+              <h2>購入者</h2>
+              <div class="person">
+                <p>お電話番号:{{checkdata[0].tel}}</p>
+                <p>メールアドレス：{{checkdata[0].mail}}</p>
+                <p>合計金額：{{goukei}}円</p>
+              </div>
+            </div>
             <br>
-            <h2>お届け先情報</h2>
-            <tr>
-              <td>{{checkdata[0].country ? checkdata[0].country:''}}</td>
-              <td>{{checkdata[0].address_01 ? checkdata[0].address_01:''}}</td>
-              <td>{{checkdata[0].address_02 ? checkdata[0].address_02:''}}</td>
-              <td>{{checkdata[0].address_03 ? checkdata[0].address_03:''}}</td>
-              <td>{{checkdata[0].address_04 ? checkdata[0].address_04:''}}</td>
-              <td>{{checkdata[0].address_05 ? checkdata[0].address_05:''}}</td>
-            </tr>
+            <div class="mboss">
+              <h2>お届け先情報</h2>
+              <div class="person">
+                <p>国名：{{checkdata[0].country ? checkdata[0].country:''}}</p>
+                <tr>
+                  <td>住所：{{checkdata[0].address_01 ? checkdata[0].address_01:''}}</td>
+                  <td>{{checkdata[0].address_02 ? checkdata[0].address_02:''}}</td>
+                  <td>{{checkdata[0].address_03 ? checkdata[0].address_03:''}}</td>
+                  <td>{{checkdata[0].address_04 ? checkdata[0].address_04:''}}</td>
+                  <td>{{checkdata[0].address_05 ? checkdata[0].address_05:''}}</td>
+                </tr>
+              </div>
+            </div>
             <br><br>
-            <h2>購入情報</h2>
-            <p>{{buytype[checkdata[0].buy_type]}}</p>
-            <p v-if="checkdata[0].buy_type==1">カード番号:{{checkdata[1].card_num}}</p>
-            <p v-if="checkdata[0].buy_type==1">カード有効期限(年):{{years[checkdata[1].year]}}</p>
-            <p v-if="checkdata[0].buy_type==1">カード有効期限(月):{{months[checkdata[1].month]}}</p>
-            <p v-if="checkdata[0].buy_type==1">セキュリティ番号:{{checkdata[1].card_sec}}</p>
+            <div class="mboss">
+              <h2>購入方法</h2>
+              <div class="person">
+                <p>{{buytype[checkdata[0].buy_type]}}</p>
+                <p v-if="checkdata[0].buy_type==1">カード番号：{{checkdata[1].card_num}}</p>
+                <p v-if="checkdata[0].buy_type==1">カード有効期限(年)：{{years[checkdata[1].year]}}</p>
+                <p v-if="checkdata[0].buy_type==1">カード有効期限(月)：{{months[checkdata[1].month]}}</p>
+                <p v-if="checkdata[0].buy_type==1">セキュリティ番号：{{checkdata[1].card_sec}}</p>
+              </div>
+            </div>
           </div>
+          <br>
           <hr>
           <br>
           <div id="item_box0">
-            <h1>購入商品確認</h1>
+            <h1 id="buy_check">購入商品確認</h1>
               <br>
               <div id="item_box">
                 <div class="buy_product" flat v-for="(item, index) in getcartdata" :key="index" >
@@ -38,19 +53,17 @@
                   </div>
                   <div style="heigh: 150px;" id="moji">
                     <div id="product_name">{{item.product_name}}</div>
+                    <div id="border"></div>
                     <div id="product_price">¥{{item.price}}</div>
                   </div>
                 </div>
               </div>
-              <!-- <p>商品名：{{getcartdata[0].product_name}}</p>
-              <p>購入される個数：{{getcartdata[0].count}}個</p>
-              <p>単価：{{getcartdata[0].price}}円</p> -->
           </div>
           <hr>
-          <div>
+          <div id="btn_box" style="margin-left: 28px;">
             <v-layout row wrap justify-end>
-              <v-btn color="success" @click="buyReq">OK</v-btn>
-              <v-btn color="red" style="color: #fff;" @click="$router.push('/customer/cart/cart')">キャンセル</v-btn>
+              <v-btn color="success" @click="buyReq" class="btn">OK</v-btn>
+              <v-btn color="grey lighten-3" @click="$router.push('/customer/cart/cartBuy')" style="margin-left: 20px;" class="btn">キャンセル</v-btn>
             </v-layout>
           </div>
         </div>
@@ -79,7 +92,7 @@ export default {
     async buyReq(){
       var buydata = [
         {
-          // name : this.checkdata[0].sei + this.checkdata[0].mei,
+          name : this.checkdata[0].name,
           tel : this.checkdata[0].tel,
           mail : this.checkdata[0].mail,
           country  : this.checkdata[0].country,
@@ -127,33 +140,69 @@ export default {
     background-color: #fff;
     margin: 20px auto;
   }
-
   #box{
     width:1000px;
+
+  }
+  #boss{
+    width:600px;
+    margin-left: 75px;
+  }
+  .mboss{
+    padding: 20px;
+  }
+  .person{
+    margin-left: 40px;
+  }
+  #buy_check{
+    margin-left: 75px;
+    padding: 0;
   }
   #item_box{
-    width: 100%;
+    width: 870px;
     display: flex;
     flex-wrap: wrap;
-    padding-bottom: 50px;
+    margin: 0 auto;
   }
   #item_box0{
-    width: 1100px;
-    padding-bottom: 30px;
+    width: 1000px;
+    padding-bottom: 40px;
   }
   .product_img{
-    width: 250px;
-    height: 250px;
+    width: 268px;
+    height: 268px;
+    border-bottom: #989898 solid 3px;
+    border: 2px;
   }
   .buy_product{
-    width: 250px;
-    height: 300px;
-    margin: 0 40px 90px 40px; 
+    background-color: #eeffee;
+    width: 270px;
+    height: 327.5px;
+    margin: 20px 10px 0px 10px ; 
+    border: #ddd solid 1px;
+    border-radius: 2px;
+  }
+  #border{
+    border: #875008 dotted 1px;
+    margin-right:5px;
   }
   #moji{
+    text-align: center;
     font-size: 15px;
+    padding: 5px 0 5px 5px;
   }
-  #product_hr{
-    margin-top: 1px;
+  #product_name{
+    margin:2.5px;
+  }
+  #product_price{
+    margin:2.5px;
+  }
+  #btn_box{
+    width: 220px;
+    padding-top: 20px;
+    margin-right: 50px;
+  }
+  .btn{
+    width: 100px;
   }
 </style>
