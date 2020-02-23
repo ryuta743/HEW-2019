b88a59487497869984037399bd686e87c697aa23
katomasato <hankun0127taktak@gmail.com>
2020-02-24 00:22:02 +0900

    工房詳細メール送信機能

--- hew2020/store/mail.js
+++ hew2020/store/mail.js
@@ -43,5 +43,22 @@ export const actions = {
         const reply_mail =  await this.$axios.$get(`/api/send_mail?to=${to}&subject=${subject}&mail=${mail}`)
         console.log(reply_mail)
     },
-
+    async　new_user_mail({commit},{payload}){
+        const to = payload.mail
+        const subject = payload.name+'様の新規登録が完了しました';
+        const mail = '天職へようこそ！！'+payload.name+'様の登録が完了いたしましたのでご連絡させていただきました。何かご不明な点や質問などございましたら、サイトのお問い合わせからご連絡ください。';
+        console.log('ここからだ')
+        console.log(mail)
+        const reply_mail =  await this.$axios.$get(`/api/send_mail?to=${to}&subject=${subject}&mail=${mail}`)
+        console.log(reply_mail)
+    },
+    async　request_mail({commit},{mail_data}){
+        console.log('storeまできたべ')
+        console.log(mail_data)
+        const to = mail_data.work_mail
+        const subject = mail_data.your_mail+'からのお問い合わせ'
+        const mail = mail_data.mail_text
+        const reply_mail =  await this.$axios.$get(`/api/send_mail?to=${to}&subject=${subject}&mail=${mail}`)
+        console.log(reply_mail)
+    },
 }
