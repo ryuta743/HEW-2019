8220d3681057fb5801bdaf3540197137771ed39d
katomasato <hankun0127taktak@gmail.com>
2020-03-02 16:08:08 +0900

    日にちところフォーマット修正

--- hew2020/store/buy.js
+++ hew2020/store/buy.js
@@ -1,9 +1,10 @@
 import moment from '~/plugins/moment';
 
-export const state = () =>({
+
+export const state = () => ({
     checkdata: null,
     buy_data: null,
-    buyhis_data:{},
+    buyhis_data: {},
 })
 
 export const getters = {
@@ -13,15 +14,15 @@ export const getters = {
 }
 
 export const mutations = {
-    setCheckdata(state,checkdata){
+    setCheckdata(state, checkdata) {
         state.checkdata = checkdata;
         console.log(checkdata)
     },
-    setBuy_data(state,buy_data){
+    setBuy_data(state, buy_data) {
         state.buy_data = buy_data;
         console.log(buy_data)
     },
-    setBuyhis_data(state,buyhis_data){
+    setBuyhis_data(state, buyhis_data) {
         state.buyhis_data = buyhis_data;
         console.log(buyhis_data)
     },
@@ -29,14 +30,22 @@ export const mutations = {
 
 export const actions = {
     // 商品購入確認
-    buycheck({commit},{buy_data}){
+    buycheck({
+        commit
+    }, {
+        buy_data
+    }) {
         console.log('storeきたよ')
         console.log(buy_data)
         commit("setCheckdata", buy_data);
     },
 
     // 商品購入処理
-    async insert_buy({commit},{buydata}){
+    async insert_buy({
+        commit
+    }, {
+        buydata
+    }) {
         console.log('購入するためのstoreきたよ')
         console.log(buydata)
 
@@ -44,15 +53,15 @@ export const actions = {
 
         // 工房の種類を求めるfor文
         var kind = [];
-        for(var c=0; c<buydata[1].cartdata.length; c++){
-            if(kind.indexOf(buydata[1].cartdata[c].shop_id) === -1){
+        for (var c = 0; c < buydata[1].cartdata.length; c++) {
+            if (kind.indexOf(buydata[1].cartdata[c].shop_id) === -1) {
                 kind.indexOf(buydata[1].cartdata[c].shop_id)
                 kind.push(buydata[1].cartdata[c].shop_id)
             }
         }
 
         // 注文一覧テーブルinsert
-        for(var a=0; a<kind.length; a++){
+        for (var a = 0; a < kind.length; a++) {
             var shop_id = buydata[1].cartdata[a].shop_id
             var now = moment().format('YYYY-MM-DD')
             var user_id = buydata[2].user_data.user_data.user_id
@@ -69,7 +78,7 @@ export const actions = {
         }
 
         // 注文明細テーブルinsert
-        for(var i = 0; i<buydata[1].cartdata.length ; i++){
+        for (var i = 0; i < buydata[1].cartdata.length; i++) {
             var shop_id = buydata[1].cartdata[i].shop_id
             var now = moment().format('YYYY-MM-DD')
             var country = buydata[0].country
@@ -87,11 +96,11 @@ export const actions = {
             var count = buydata[1].cartdata[i].count
             var buy_detail = await this.$axios.$get(`http://133.18.194.128:5000/buy/buy_details?order_id=${saleid}&shop_id=${shop_id}&user_id=${user_id}&user_name=${user_name}&product_id=${product_id}&product_name=${product_name}&price=${price}&count=${count}`);
             //LINEAPI処理
-            console.log('buy_detai:'+buy_detail)
+            console.log('buy_detai:' + buy_detail)
         }
 
         // 購入処理完了後の商品個数を減らすやつ
-        for(var z=0; z<buydata[1].cartdata.length; z++){
+        for (var z = 0; z < buydata[1].cartdata.length; z++) {
             var p_id = buydata[1].cartdata[z].product_id
             var now_count = await this.$axios.$get(`http://133.18.194.128:5000/product/now_count?p_id=${p_id}`);
             console.log(now_count)
@@ -100,9 +109,9 @@ export const actions = {
             var upd_count = await this.$axios.$get(`http://133.18.194.128:5000/product/upd_count?new_count=${new_count}&p_id=${p_id}`);
             console.log(upd_count)
             const line_id = await this.$axios.$get(`http://133.18.194.128:5000/linebot/get_lineid?shop_id=${buydata[1].cartdata[z].shop_id}`);
-            console.log('lineID',line_id)
-            if(line_id.length > 0){
-                if(buydata[1].cartdata[z].safety >= new_count){
+            console.log('lineID', line_id)
+            if (line_id.length > 0) {
+                if (buydata[1].cartdata[z].safety >= new_count) {
                     this.$axios.$get(`https://hidden-woodland-14194.herokuapp.com/sendm?shop_id=${buydata[1].cartdata[z].shop_id}&image=${buydata[1].cartdata[z].product_img_origin}&product_number=${buydata[1].cartdata[z].product_number}&product_name=${buydata[1].cartdata[z].product_name}&line_id=${line_id[0].line_id}`);
                 }
             }
@@ -112,14 +121,21 @@ export const actions = {
         const del_cart = await this.$axios.$get(`http://133.18.194.128:5000/cart/delete_cart?user_id=${buydata[2].user_data.user_data.user_id}`);
         console.log(del_cart)
 
-        commit("setBuy_data",del_cart);
+        commit("setBuy_data", del_cart);
     },
-    async get_buy_history({commit},{user_id}){
+    async get_buy_history({
+        commit
+    }, {
+        user_id
+    }) {
         console.log('購入履歴取得');
         console.log(user_id)
         const get_buy_his = await this.$axios.$get(`http://133.18.194.128:5000/buy/get_buy_history?user_id=${user_id}`);
         console.log('おかえり');
         console.log(get_buy_his)
-        commit('setBuyhis_data',get_buy_his)
+        for (var a = 0; a < get_buy_his.length; a++) {
+            get_buy_his[a].buy_date = moment(get_buy_his[a].buy_date).format("YYYY年MM月DD日");
+        }
+        commit('setBuyhis_data', get_buy_his)
     },
 }
